b9aa82c2b65f02e82565cc21856a5e433c811c5c
Minamaher-sw <mosadefmena185@gmail.com>
2024-12-18 13:10:55 +0200

    add project1

---
+++ Proect1/main.c
@@ -0,0 +1,12 @@
+/* Include stdio.h library
+   To use printf and scanf function */
+#include <stdio.h>
+
+/* define the main function */
+void main(void)
+{
+   int a =10;
+   int b =15 ;
+   printf("%d\n", a+b) ;
+    printf("Mina Maher");
+}
